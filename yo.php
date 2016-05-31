7bcd1ec2c0d2e0d89b7cce10b5a811bf3ff2865c
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-31 16:01:28 -0700

    Update yo.php

--- yo.php
+++ yo.php
@@ -1,1 +1,6 @@
-
+<?php
+$database = new SQLite3('myDatabase.sqlite');
+$query_string = "CREATE TABLE IF NOT EXISTS numbers(password STRING)";
+$database->exec($query_string);
+$query_string = "INSERT INTO numbers VALUES('1')";
+$database->exec($query_string);
