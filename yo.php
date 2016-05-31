168adfbf26518260c0005a94e3e7841daab832b6
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-31 16:54:58 -0700

    Update yo.php

--- yo.php
+++ yo.php
@@ -4,3 +4,6 @@ $query_string = "CREATE TABLE IF NOT EXISTS numbers(password STRING)";
 $database->exec($query_string);
 $query_string = "INSERT INTO numbers VALUES('1')";
 $database->exec($query_string);
+$result = $database->query('SELECT * FROM users');
+echo $result;
+?>
