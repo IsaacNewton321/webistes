85e74dac61af5257d1316b89aa6d47c6273e3f52
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-08-16 15:14:58 -0700

    Delete yo.php

--- yo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$database = new SQLite3('myDatabase.sqlite');
-$query_string = "CREATE TABLE IF NOT EXISTS numbers(password STRING)";
-$database->exec($query_string);
-$query_string = "INSERT INTO numbers VALUES('1')";
-$database->exec($query_string);
-$result = $database->query('SELECT * FROM users');
-echo $result;
-?>
